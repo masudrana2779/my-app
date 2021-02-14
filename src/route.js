1e5ee2e00526b4759a1dd7a5f5fc1f653dbf6f27
Masud Rana <masudrana2779@gmail.com>
2021-02-14 18:05:55 +0600

    pro add

--- src/route.js
+++ src/route.js
@@ -1,6 +1,6 @@
-export const home = '/home';
-export const about = '/about';
-export const services = '/services';
-export const blog = '/blog';
-export const blogDetails = `/blog`;
-export const contact = '/contact';
+export const home = '/my-app/home';
+export const about = '/my-app/about';
+export const services = '/my-app/services';
+export const blog = '/my-app/blog';
+export const blogDetails = `/my-app/blog`;
+export const contact = '/my-app/contact';
